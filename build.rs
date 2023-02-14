32fbbc8d380d121c437e391a426eb2ce1ccf88d5
Maxime Bouillot <maxime.bouillot@withings.com>
2023-02-14 17:11:40 +0100

    Disable programming tools and files.

--- build.rs
+++ build.rs
@@ -10,6 +10,8 @@ fn main() {
         .compile("cancellable_thread");
 
     Command::new("git").args(&["clone", "https://github.com/Arkaeriit/ASCminiForth.git"]).status().unwrap();
+    Command::new("sed").args(&["-i", "ASCminiForth/amf_config.h", "-e", "s:AMF_FILE.*:AMF_FILE 0:"]).status().unwrap(); // Yes, I use GNU sed syntax, this project is too stupid for me to care about portability
+    Command::new("sed").args(&["-i", "ASCminiForth/amf_config.h", "-e", "s:AMF_PROGRAMMING_TOOLS.*:AMF_PROGRAMMING_TOOLS 0:"]).status().unwrap();
     Command::new("make").current_dir("./ASCminiForth").arg("libamForth.a").status().unwrap();
     Command::new("cp").args(&["./ASCminiForth/libamForth.a", "./", "-f"]).status().unwrap();
 }

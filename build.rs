068ce8f84f96972e5892e232679e88547036d02f
Maxime Bouillot <maxbouillot@gmail.com>
2023-02-15 15:35:41 +0000

    Updated build script.
    The name were a bit outdated in it.

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use std::process::Command;
 
 fn main() {
     println!("cargo:rustc-link-search=.");
-    println!("cargo:rustc-link-lib=amForth");
+    println!("cargo:rustc-link-lib=amforth");
 
     println!("cargo:rerun-if-changed=src/cancellable_thread.c");
     cc::Build::new()
@@ -12,7 +12,7 @@ fn main() {
     Command::new("git").args(&["clone", "https://github.com/Arkaeriit/ASCminiForth.git"]).status().unwrap();
     Command::new("sed").args(&["-i", "ASCminiForth/amf_config.h", "-e", "s:AMF_FILE.*:AMF_FILE 0:"]).status().unwrap(); // Yes, I use GNU sed syntax, this project is too stupid for me to care about portability
     Command::new("sed").args(&["-i", "ASCminiForth/amf_config.h", "-e", "s:AMF_PROGRAMMING_TOOLS.*:AMF_PROGRAMMING_TOOLS 0:"]).status().unwrap();
-    Command::new("make").current_dir("./ASCminiForth").arg("libamForth.a").status().unwrap();
-    Command::new("cp").args(&["./ASCminiForth/libamForth.a", "./", "-f"]).status().unwrap();
+    Command::new("make").current_dir("./ASCminiForth").arg("libamforth.a").status().unwrap();
+    Command::new("cp").args(&["./ASCminiForth/libamforth.a", "./", "-f"]).status().unwrap();
 }
 

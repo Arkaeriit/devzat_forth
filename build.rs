5e886fde419a9e138fb719eec238f990aaa43c4c
Maxime Bouillot <maxbouillot@gmail.com>
2024-08-27 21:35:08 +0200

    Use CFLAGS compatible with newer Rust compilers.

--- build.rs
+++ build.rs
@@ -12,7 +12,7 @@ fn main() {
     Command::new("git").args(&["clone", "https://github.com/Arkaeriit/ASCminiForth.git"]).status().unwrap();
     Command::new("sed").args(&["-i", "ASCminiForth/amf_config.h", "-e", "s:AMF_FILE.*:AMF_FILE 0:"]).status().unwrap(); // Yes, I use GNU sed syntax, this project is too stupid for me to care about portability
     Command::new("sed").args(&["-i", "ASCminiForth/amf_config.h", "-e", "s:AMF_PROGRAMMING_TOOLS.*:AMF_PROGRAMMING_TOOLS 0:"]).status().unwrap();
-    Command::new("make").current_dir("./ASCminiForth").arg("libamforth.a").status().unwrap();
+    Command::new("make").current_dir("./ASCminiForth").env("CFLAGS", "-fPIE").arg("libamforth.a").status().unwrap();
     Command::new("cp").args(&["./ASCminiForth/libamforth.a", "./", "-f"]).status().unwrap();
 }
 
